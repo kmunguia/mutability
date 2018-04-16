e25a77f323d77495217565deed8533710c8377d5
KassM <djl047km>
2018-04-16 01:12:13 -0500

    part 0 is complete. Double-click feature is removed, dead code is removed

--- elm-todo/Todo.elm
+++ elm-todo/Todo.elm
@@ -76,8 +76,6 @@ to them.
 -}
 type Msg
     = UpdateNewEntryField String
-    | EditingEntry Int Bool
-    | UpdateEntry Int String
     | Add
     | Delete Int
     | DeleteAllCompleted
@@ -101,29 +99,6 @@ update msg model =
 
         UpdateNewEntryField str ->
             { model | newEntryField = str }
-
-        EditingEntry id isEditing ->
-            let
-                updateEntry t =
-                    if t.id == id then
-                        { t | editing = isEditing }
-                    else
-                        t
-
-                focus =
-                    Dom.focus ("todo-" ++ toString id)
-            in
-                { model | entries = List.map updateEntry model.entries }
-
-        UpdateEntry id task ->
-            let
-                updateEntry t =
-                    if t.id == id then
-                        { t | description = task }
-                    else
-                        t
-            in
-                { model | entries = List.map updateEntry model.entries }
 
         Check id isCompleted ->
             let
@@ -229,7 +204,7 @@ viewKeyedEntry todo =
 viewEntry : Entry -> Html Msg
 viewEntry todo =
     li
-        [ classList [ ( "completed", todo.completed ), ( "editing", todo.editing ) ] ]
+        [ classList [ ( "completed", todo.completed ) ] ]
         [ div
             [ class "view" ]
             [ input
@@ -240,7 +215,7 @@ viewEntry todo =
                 ]
                 []
             , label
-                [ onDoubleClick (EditingEntry todo.id True) ]
+                []
                 [ text todo.description ]
             , button
                 [ class "destroy"
@@ -248,16 +223,6 @@ viewEntry todo =
                 ]
                 []
             ]
-        , input
-            [ class "edit"
-            , value todo.description
-            , name "title"
-            , id ("todo-" ++ toString todo.id)
-            , onInput (UpdateEntry todo.id)
-            , onBlur (EditingEntry todo.id False)
-            , onEnter (EditingEntry todo.id False)
-            ]
-            []
         ]
 
 
@@ -313,8 +278,7 @@ viewControlsClear entriesCompleted =
 infoFooter : Html msg
 infoFooter =
     footer [ class "info" ]
-        [ p [] [ text "Double-click to edit a todo" ]
-        , p []
+        [ p []
             [ text "Written by "
             , a [ href "https://github.com/evancz" ] [ text "Evan Czaplicki" ]
             ]
